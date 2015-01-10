1bb924ac1ad101be957029d29cfdd834583ffb1d
Sam Khan <samk1200@gmail.com>
2015-01-10 20:50:36 +0000

    added mobile views

--- mcmobile/urls.py
+++ mcmobile/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('mcmobile.views',
     url(r'^itinerary', 'schedule', name='schedule_mob'),
     url(r'^committees-list', 'committees', name='committees_mob'),
-    url(r'^socialmedia', 'social_media', name='socialmedia_mob'),
+    url(r'^social', 'social_media', name='socialmedia_mob'),
+    url(r'^sponsors', 'sponsors', name='sponsors_mob'),
+    url(r'^food', 'food', name='food_mob'),
+    url(r'^contact', 'contact', name='contact_mob'),
+    url(r'^map', 'map', name='map_mob'),
 )
